2e880510c2482e4b157808236d2fd8e78e646a91
Anneliese <62834770+walsha61@users.noreply.github.com>
2020-04-02 18:13:02 +0100

    Database sql dump file upload
    Uploaded sql dump file and guide for creating the database

---
+++ sql_database/README.md
@@ -0,0 +1,106 @@
+# SQL Database
+> A guide for connecting a python project to an SQL Database
+
+One of the most crucial things in your open source project is the `README.md`
+file. This repository has a ready-to-copy-paste template you can use for all
+your projects.
+
+## Installing SQL Server
+
+Go to the link below and download the MySQL Community Server: 
+
+```shell
+[https://dev.mysql.com/downloads/mysql/]
+```
+
+For macOS download the version titled:
+```shell
+macOS 10.15 (x86, 64-bit), DMG Archive
+```
+For Windows, download the version titled:
+```shell
+Windows (x86, 64-bit), ZIP Archive
+```
+
+## Installing SQLWorkbench
+
+Download the SQLWorkbench from the following link:
+
+```shell
+[https://www.mysql.com/products/workbench/]
+```
+
+Click the "Download Now" button, then navigate to the botton of the page. Select your operating system and click download.
+
+## Start the server
+
+1. Navigate to System Preferences on your device. 
+2. Click the MySQL icon. 
+3. CLick *initialise database*.
+4. Enter a password for the database root user (password123)
+5. Click *Use Legacy Password Encryption* and press ok
+6. Click *Start MySQL Server*
+
+
+## Setup a new connection
+
+Open the MySQL Workbench:
+1. Click '+' icon beside MySQL Connections
+2. Name the connection "Test Connection"
+3. Beside password, click *Store in Keychain* and enter password123
+4. Click *Test Connection* to check the connection
+
+
+## Import Database from sqldump file
+
+1. From the bar at the top of the application, select Server > Data Import
+2. Choose *Import from Self-Contained File* and select the ``sqldump.sql`` file
+3. Click *Start Import* in the bottom right of the screen
+
+
+## Connecting Python to SQL Server
+
+The python editor PyCharm can be downloaded [here](https://www.jetbrains.com/pycharm/).
+
+Include the connector library:
+1. Navigate: PyCharm > Preferences > Project: PythonMySQL > Project Interpreter
+2. Click the *+* icon in the bottom left corner.
+3. Search *mysql-connector* and click *install package*
+
+For other IDEs, run the following on the command line:
+
+```shell
+$ pip install mysql-connector-python
+```
+
+In your python script, run the following code to connect to your SQL server:
+
+```shell
+import mysql.connector
+
+mydb = mysql.connector.connect(
+    host = "localhost",
+    user = "root",
+    passwd = "password123",
+    database = "robo_db"
+)
+```
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ sql_database/sqldump.sql
@@ -0,0 +1,101 @@
+CREATE DATABASE  IF NOT EXISTS `robo_db` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `testdb`;
+-- MySQL dump 10.13  Distrib 8.0.19, for macos10.15 (x86_64)
+--
+-- Host: 127.0.0.1    Database: testdb
+-- ------------------------------------------------------
+-- Server version	8.0.19
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `robo_tb`
+--
+
+DROP TABLE IF EXISTS `robo_tb`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `robo_tb` (
+  `id` int DEFAULT NULL,
+  `salt` text,
+  `hash_pword` text,
+  `curr_room` int DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `robo_tb`
+--
+
+LOCK TABLES `robo_tb` WRITE;
+/*!40000 ALTER TABLE `robo_tb` DISABLE KEYS */;
+INSERT INTO `robo_tb` VALUES (1,'$2b$14$y1.Hk0K5ZlOddaVF1oWP6.','$2b$14$y1.Hk0K5ZlOddaVF1oWP6.NPRLt8YPfDHeOa2DkmU63E1Wvii5FOy',0),(2,'$2b$14$ZmesmMUmDrSdaFLWpMf6Fe','$2b$14$ZmesmMUmDrSdaFLWpMf6FeWmuF3eB/WBFxC6HUKTvgfdfaGWWBOBe',0),(3,'$2b$14$IVxGsAYb8ycDW1JnTyDTuO','$2b$14$IVxGsAYb8ycDW1JnTyDTuOUjyIFwgttZeGafTGV6i1Gj3qo41VXE6',0),(4,'$2b$14$/Dq6GdyDWR9CrYzkrbnUW.','$2b$14$/Dq6GdyDWR9CrYzkrbnUW.qAFqSuvcubQq/MXn1sJgMhfn8h29OLW',0);
+/*!40000 ALTER TABLE `robo_tb` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `room_tb`
+--
+
+DROP TABLE IF EXISTS `room_tb`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `room_tb` (
+  `id` int DEFAULT NULL,
+  `status` int DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `room_tb`
+--
+
+LOCK TABLES `room_tb` WRITE;
+/*!40000 ALTER TABLE `room_tb` DISABLE KEYS */;
+INSERT INTO `room_tb` VALUES (1,0),(2,0),(3,0),(4,0),(5,0);
+/*!40000 ALTER TABLE `room_tb` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `staff_tb`
+--
+
+DROP TABLE IF EXISTS `staff_tb`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `staff_tb` (
+  `name` varchar(255) DEFAULT NULL,
+  `salt` varchar(29) DEFAULT NULL,
+  `hash_pword` varchar(60) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `staff_tb`
+--
+
+LOCK TABLES `staff_tb` WRITE;
+/*!40000 ALTER TABLE `staff_tb` DISABLE KEYS */;
+INSERT INTO `staff_tb` VALUES ('Gwen','$2b$14$getcgbCfJbfBPe5BgRrcGO','$2b$14$getcgbCfJbfBPe5BgRrcGOf/LhvnalJ6GkUa5ZJL8Z3OtZQ1wBCPq'),('Nico','$2b$14$xE0hfDYcUE5B0y2YCIOhue','$2b$14$xE0hfDYcUE5B0y2YCIOhueSi1m9Zbv8lvwHMGuePh0xk6zJxPkHBK'),('Amiel','$2b$14$iQeWFL/oIeJw8nxcsMDPO.','$2b$14$iQeWFL/oIeJw8nxcsMDPO.JOKaKE9MTx9pz9mmfi9cSyLWXESQRBW'),('Bellard','$2b$14$21KgNRRVsCG8yHwmeerP5u','$2b$14$21KgNRRVsCG8yHwmeerP5uraOPoJBU5wQ03UuHsaco6.zaBwARy4O');
+/*!40000 ALTER TABLE `staff_tb` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2020-04-02  8:21:04
